8e1d930f0f91d507430a2a945e4df51816e853db
Igor <igorsvk16@gmail.com>
2026-01-02 14:56:35 +0300

    feat: add more functional

--- script.js
+++ script.js
@@ -2,21 +2,23 @@ const Gameboard = (() => {
     const board = Array(9).fill(null);
 
     function getBoard() {
-        return board;
+        return board.slice();
     }
-
     function setMark(index, mark) {
-        if (board[index] === null && index >= 0 && index <= 8) {
-            return board[index] = mark;
+        if (index >= 0 && index <= 8 && board[index] === null) {
+            board[index] = mark;
+            return true;
+        } else {
+            return false;
+        }    
+    }
+    function reset() {
+        for (let i = 0; i < board.length; i++) {
+            board[i] = null;
         }
-         
     }
 
-    function getBoard() {}
-
-    
-
-    return { getBoard, setMark }
+    return { getBoard, setMark, reset }
 })();
 
 function Player(name, mark) {
@@ -28,22 +30,90 @@ function GameController() {
     let currentPlayerIndex = 0;
     let gameOver = false;
 
+    function printBoard(board) {
+        const cell = (v) => (v === null ? "." : v);
+
+        console.log(board.slice(0, 3).map(cell).join(" "))
+        console.log(board.slice(3, 6).map(cell).join(" "))
+        console.log(board.slice(6, 9).map(cell).join(" "))
+    }
+
+    function playRound(index) {
+        if (gameOver) {
+            console.log('The game is over. Please reset to start a new game.');
+            return;
+        }
+        const player = getActivePlayer();
+        const placed = Gameboard.setMark(index, player.mark);
+
+        if (!placed) {
+            console.log(`Invalid move`);
+            return;
+        }
+        const board = Gameboard.getBoard();
+        printBoard(board);
+        const winnerMark = checkWinner(board);
+        if (winnerMark) {
+            gameOver = true;
+            console.log(`${player.name} with mark ${winnerMark} wins!`);
+            return;
+        }
+        const isDraw = checkDraw(board);
+        if (isDraw === true) {
+            console.log('The game is a draw!');
+            return;
+        }
+
+        switchPlayer();
+        console.log(`It's now ${getActivePlayer().name}'s turn.`);
+    }
+
+    const WIN_LINES = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+
+    function checkWinner(board) {
+        
+        for (const line of WIN_LINES) {
+            const [a, b, c] = line;
+            if (board[a] !== null && board[a] === board[b] && board[a] === board[c]) {
+                return board[a];
+            }
+        }
+        return null;
+    }
+    
+    function checkDraw(board) {
+        if (board.every(cell => cell !== null)) {
+            gameOver = true
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function switchPlayer() {
+        currentPlayerIndex = 1 - currentPlayerIndex;
+    }
+
     function getActivePlayer() {
         return players[currentPlayerIndex];
     }
 
     function resetGame() {
-        for (let i = 0; i < board.length; i++) {
-            board[i] = null;
-        }
+        Gameboard.reset();
+        currentPlayerIndex = 0;
+        gameOver = false;
     }
 
-    function playRound(index) {
-
-    }
-    
-    function checkDraw(board) {
-
-    }
-
+    return { getActivePlayer, playRound, resetGame }
 }
+
+const game = GameController();
